6f48d51648d4c7bfb75dd8f74f86de4c75dd646a
tomohatsu <tomohatsu-design@outlook.com>
2022-02-05 07:31:47 +0900

    20220205-am

--- WordPressTheme/single.php
+++ WordPressTheme/single.php
@@ -61,91 +61,64 @@
         <span class="u-desktop">関連記事</span>
       </h2>
       <div class="p-recommend-article__content p-cards-4">
-      <?php
-        //カスタム投稿タイプを取得 
-        $post_type = get_post_type( $post );
-        //タクソノミースラッグ取得(単数)
-        $taxonomy_slug = array_keys(get_the_taxonomies());
-        // ターム情報を取得
-        $term = get_the_terms($post->ID, $taxonomy_slug);
 
-        var_dump($post_type);
-        var_dump($taxonomy_slug);
-        var_dump($term);
-      ?>
+        
       <?php
-        $args = array(
-          'numberposts' => 8, //８件表示(デフォルトは５件)
-          'post_type' => $post_type, //カスタム投稿タイプ名
-          'taxonomy' => $taxonomy_slug, //タクソノミー名
-          'term' => $term->slug, //ターム名 ←ここが追加
-          'orderby' => 'rand', //ランダム表示
-          'post__not_in' => array($post->ID); //表示中の記事を除外
-      ?>
-
-      <?php $myPosts = get_posts($args); if($myPosts) : ?>
-      <?php foreach($myPosts as $post) : setup_postdata($post); ?>
-        <p><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?><?php the_title(); ?></a></p>
-      <?php endforeach; ?>
-      <?php else : ?>
-        <p>関連アイテムはまだありません。</p>
-      <?php endif; wp_reset_postdata(); ?>
+      //カスタム投稿タイプを取得 
+      $post_type = get_post_type( $post );
+      //タクソノミースラッグ取得(単数)
+      $taxonomy_slug = array_keys(get_the_taxonomies());
+      //タクソノミー情報取得
+      $taxonomy = get_taxonomy($taxonomy_slug[0]);
+      //タクソノミー名取得
+      $taxonomy_name = $taxonomy->name;//カスタムタクソノミースラッグ
+      $taxonomy_terms_cat = wp_get_object_terms($post->ID, $taxonomy_name);
+      $taxonomy_terms_cat_slug = $taxonomy_terms_cat[0]->slug; //タームスラッグ
       
+        $my_query = new WP_Query(
+          array(
+            'posts_per_page' => 4, // 表示件数を指定
+            'post_type' => $post_type, //カスタム投稿タイプ名
+            'taxonomy' => $taxonomy_name, //タクソノミー名スラッグ
+            'term' => $taxonomy_terms_cat_slug, //ターム名
+            'orderby' => 'rand', //ランダム表示
+            'post__not_in' => array($post->ID) //表示中の記事を除外
+            )
+          );
+      ?>
 
+      <?php if ( $my_query->have_posts() ) : ?>
+      <?php while ( $my_query->have_posts() ) : ?>
+      <?php $my_query->the_post(); ?>
+        
         <a class="p-cards-4__item p-card-small">
           <figure class="p-card-small__img">
-            <img src="./assets/images/single-blog/single-blog1.jpg" alt="写真：水の入ったグラスとノートパソコン">
+          <?php if (has_post_thumbnail()): ?>
+                <?php the_post_thumbnail( 'full' ); ?>
+              <?php else: ?>
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/common/no-image.jpg" alt="no image">
+              <?php endif; ?>
           </figure>
           <div class="p-card-small__body">
-            <h3 class="p-card-small__title">タイトルが入ります。タイトルが入ります。</h3>
-            <p class="p-card-small__excerpt u-mobile">説明文が入ります。説明文が入ります。説明文が入ります。</p>
+            <h3 class="p-card-small__title"><?php the_title(); ?></h3>
+            <p class="p-card-small__excerpt u-mobile"><?php the_excerpt(); ?></p>
           </div>
           <div class="p-card-small__info">
-            <span class="p-card-small__label c-label-card-yellow">カテゴリ</span>
+          <?php $term = get_the_terms($post->ID, $taxonomy_name); ?>
+            <?php if ( !empty($term)){; ?>
+              <span class="p-card-small__label c-label-card-yellow"><?php echo $term[0]->name; ?></span>
+            <?php }; ?>
             <time class="p-card-small__date c-date-blog" datetime="2021-07-20">2021.07.20</time>
           </div>
         </a>
 
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(); ?>
+      <?php else: ?>
+      <!-- 投稿が無い場合の処理 -->
+      <p>現在、関連記事はありません</p>
+      <?php endif; ?>
 
-        <a class="p-cards-4__item p-card-small">
-          <figure class="p-card-small__img">
-            <img src="./assets/images/single-blog/single-blog2.jpg" alt="写真：沢山のモニターがある部屋">
-          </figure>
-          <div class="p-card-small__body">
-            <h3 class="p-card-small__title">タイトルが入ります。タイトルが入ります。</h3>
-            <p class="p-card-small__excerpt u-mobile">説明文が入ります。説明文が入ります。説明文が入ります。</p>
-          </div>
-          <div class="p-card-small__info">
-            <span class="p-card-small__label c-label-card-yellow">カテゴリ</span>
-            <time class="p-card-small__date c-date-blog" datetime="2021-07-20">2021.07.20</time>
-          </div>
-        </a>
-        <a class="p-cards-4__item p-card-small">
-          <figure class="p-card-small__img">
-            <img src="./assets/images/single-blog/single-blog3.jpg" alt="写真：タブレットを操作する男性">
-          </figure>
-          <div class="p-card-small__body">
-            <h3 class="p-card-small__title">タイトルが入ります。タイトルが入ります。</h3>
-            <p class="p-card-small__excerpt u-mobile">説明文が入ります。説明文が入ります。説明文が入ります。</p>
-          </div>
-          <div class="p-card-small__info">
-            <span class="p-card-small__label c-label-card-yellow">カテゴリ</span>
-            <time class="p-card-small__date c-date-blog" datetime="2021-07-20">2021.07.20</time>
-          </div>
-        </a>
-        <a class="p-cards-4__item p-card-small">
-          <figure class="p-card-small__img">
-            <img src="./assets/images/single-blog/single-blog4.jpg" alt="写真：お洒落な部屋のモニター">
-          </figure>
-          <div class="p-card-small__body">
-            <h3 class="p-card-small__title">タイトルが入ります。タイトルが入ります。</h3>
-            <p class="p-card-small__excerpt u-mobile">説明文が入ります。説明文が入ります。説明文が入ります。</p>
-          </div>
-          <div class="p-card-small__info">
-            <span class="p-card-small__label c-label-card-yellow">カテゴリ</span>
-            <time class="p-card-small__date c-date-blog" datetime="2021-07-20">2021.07.20</time>
-          </div>
-        </a>
       </div>
     </div>
     
--- WordPressTheme/test.php
+++ WordPressTheme/test.php
@@ -1,33 +1,29 @@
 <?php
-      $term = get_queried_object();
-      // var_dump($term);
-      $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-      $the_query = new WP_Query(
-        array( 
-          'post_type' => 'works',
-          'taxonomy' => 'works_category',
-          'term' => $term->slug,
-          'posts_per_page' => 6,
-          'paged' => $paged, // ページ番号
-        )
-      );
-    ?>
+        //カスタム投稿タイプを取得 
+        $post_type = get_post_type( $post );
+        //タクソノミースラッグ取得(単数)
+        $taxonomy_slug = array_keys(get_the_taxonomies());
+        // ターム情報を取得
+        $term = get_the_terms($post->ID, $taxonomy_slug);
+        $term_slug = $term[0] -> slug;
 
-<?php
-    $cat = get_query_var( 'works_category' ); //指定したいタクソノミーを指定
-    $the_query = new WP_Query(
-      array( 
-        'post_type' => array('works'), 
-        'tax_query' => array(
-            'relation' => 'OR',
-            array(
-                'taxonomy' => 'works_category',
-                'field' => 'slug',
-                'terms' => $cat, /* 上記で指定した変数を指定 */
-            ),
-        ),
-        'paged' => get_query_var('paged'),
-        'posts_per_page' => '3' /* 1ページに表示させたい件数 */
-        )
-      );
-    ?>
+        var_dump($post_type);
+        var_dump($taxonomy_slug);
+        var_dump($term);
+        var_dump($term_slug);
+        ?>
+        
+      <?php
+        $my_query = new WP_Query(
+          array(
+            'post_type' => $post_type, //カスタム投稿タイプ名
+            'taxonomy' => $taxonomy_slug, //タクソノミー名
+            // 'term' => $term_slug, //ターム名
+            'posts_per_page' => '4', //表示件数
+            'orderby' => 'rand', //ランダム表示
+            'post__not_in' =>array( $post->ID ), //表示中の記事を除外
+            )
+          );
+          ?>
+
+      <?php var_dump($my_query); ?>
