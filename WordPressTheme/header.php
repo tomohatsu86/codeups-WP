6fc14d41553815853245923b5c37adc7dcbb43fd
tomohatsu <tomohatsu-design@outlook.com>
2022-02-26 07:25:08 +0900

    20220226-am noindex add

--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="format-detection" content="telephone=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name=”robots” content=”noindex”>
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
